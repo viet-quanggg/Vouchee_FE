cf614fe6c1f81b275c7bc1df927810f807fe2a8c
viet-quanggg <nguyenvietquang099@gmail.com>
2024-07-22 20:12:16 +0700

    [FE][Seller] add seller ui

--- client/src/pages/Seller/SellerOrder.jsx
+++ client/src/pages/Seller/SellerOrder.jsx
@@ -1,7 +1,166 @@
 import React from "react";
 
+import { AiFillProduct } from "react-icons/ai";
+import { FaChevronRight } from "react-icons/fa6";
+
 const SellerOrder = () => {
-  return <div>SellerOrder</div>;
+  return (
+    <div>
+      {" "}
+      <div className="w-full h-full p-8 flex flex-col">
+        <div className="flex items-center space-x-2">
+          <AiFillProduct className="text-xl" />
+          <div>Sản Phẩm</div>
+          <FaChevronRight />
+          <div>Quản Lý Sản Phẩm</div>
+        </div>
+
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
+          <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 m-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Thêm sản phẩm mới
+          </button>
+          <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-6 py-3">
+                  Tên sản phẩm
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Mô tả
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Ngành hàng
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Giá bán gốc
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Giá bán giảm
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Chức năng
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th
+                  scope="row"
+                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  Apple MacBook Pro 17"
+                </th>
+                <td class="px-6 py-4">Silver</td>
+                <td class="px-6 py-4">Laptop</td>
+                <td class="px-6 py-4">$2999</td>
+                <td class="px-6 py-4">$2999</td>
+                <td class="px-6 py-4">
+                  <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                    Chỉnh sửa
+                  </a>
+                </td>
+              </tr>
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th
+                  scope="row"
+                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  Microsoft Surface Pro
+                </th>
+                <td class="px-6 py-4">White</td>
+                <td class="px-6 py-4">Laptop PC</td>
+                <td class="px-6 py-4">$1999</td>
+                <td class="px-6 py-4">$2999</td>
+
+                <td class="px-6 py-4">
+                  <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                    Chỉnh sửa
+                  </a>
+                </td>
+              </tr>
+              <tr class="bg-white dark:bg-gray-800">
+                <th
+                  scope="row"
+                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  Magic Mouse 2
+                </th>
+                <td class="px-6 py-4">Black</td>
+                <td class="px-6 py-4">Accessories</td>
+                <td class="px-6 py-4">$99</td>
+                <td class="px-6 py-4">$2999</td>
+
+                <td class="px-6 py-4">
+                  <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                    Chỉnh sửa
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <nav
+            class="flex items-center flex-column flex-wrap md:flex-row justify-between p-4"
+            aria-label="Table navigation"
+          >
+            <span class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
+              Đang hiển thị{" "}
+              <span class="font-semibold text-gray-900 dark:text-white">
+                1-10
+              </span>{" "}
+              của{" "}
+              <span class="font-semibold text-gray-900 dark:text-white">
+                1000
+              </span>
+            </span>
+            <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+              <li>
+                <a class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  Trang trước
+                </a>
+              </li>
+              <li>
+                <a class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  1
+                </a>
+              </li>
+              <li>
+                <a class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  2
+                </a>
+              </li>
+              <li>
+                <a
+                  aria-current="page"
+                  class="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+                >
+                  3
+                </a>
+              </li>
+              <li>
+                <a class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  4
+                </a>
+              </li>
+              <li>
+                <a class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  5
+                </a>
+              </li>
+              <li>
+                <a class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  Trang tiếp
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        {/* code ở đây  */}
+      </div>
+    </div>
+  );
 };
 
 export default SellerOrder;
